74ff3c367525a911babd2b45b9eb09749d96a588
fl327 <soxehli@gmail.com>
2022-08-30 18:44:56 -0400

    add sth

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,31 +1,43 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
 import com.badlogic.gdx.utils.ScreenUtils;
+import edu.cornell.gdiac.ui.UI;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+
+	Stage stage;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		JsonReader reader = new JsonReader();
+		JsonValue json = reader.parse(Gdx.files.internal("assets.json"));
+		stage = UI.genSceneGraph(json);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		ScreenUtils.clear(1, 1, 1, 1);
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height){
+		stage.getViewport().update(width, height, true);
 	}
 }
--- core/src/edu/cornell/gdiac/ui/UI.java
+++ core/src/edu/cornell/gdiac/ui/UI.java
@@ -9,10 +9,34 @@
 
 package edu.cornell.gdiac.ui;
 
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Tree;
+import com.badlogic.gdx.scenes.scene2d.ui.Widget;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonValue;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class UI {
+    public static Stage genSceneGraph(JsonValue json){
+        Stage stage = new Stage();
+        //TODO: Parse the skin
+        Skin skin = new Skin();
+        //Tree tree = new Tree(skin);
+        Button b = new Button();
+        b.setStyle(new Text);
+        stage.addActor(b);
+/*
+        JsonValue sceneGraph = json.get("scene2s");
+        if(sceneGraph==null||sceneGraph.isEmpty())
+            throw new IllegalArgumentException("corrupted json file, does not contain scene2s specs");
+        for(JsonValue actor:sceneGraph){
 
-    public void genSceneGraph readUI()
+        }
+        throw new Error("Unimplemented");
+
+ */
+        return stage;
+    }
 }
