f1c716a20eed3181ebbbea8b6fcb31943dd898fa
fl327 <fl327@cornell.edu>
2022-08-30 15:43:37 -0400

    Initial Commit

---
+++ core/src/edu/cornell/gdiac/ui/UI.java
@@ -0,0 +1,18 @@
+/*
+ * UI.java
+ *
+ * This is the class that processes .json format ui specifications to generate the scene graphs in LibGdx
+ *
+ * @author Barry Lyu
+ * @date   8/30/22
+ */
+
+package edu.cornell.gdiac.ui;
+
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonValue;
+
+public class UI {
+
+    public void genSceneGraph readUI()
+}
