79d383b233aaaba95506120d91dde3197d704c77
Miguel Amor <miguel.amor3629@gmail.com>
2023-09-28 21:08:25 -0400

    Textfield and ProgressBar parsers
    WARNING: untested

---
+++ core/src/edu/cornell/gdiac/ui/nodeParser/TextFieldParser.java
@@ -0,0 +1,27 @@
+package edu.cornell.gdiac.ui.nodeParser;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.utils.JsonValue;
+import edu.cornell.gdiac.ui.assets.AssetDirectory;
+
+import java.awt.*;
+
+public class TextFieldParser implements NodeParser {
+    @Override
+    public String getTypeKey() { return "Textfield"; }
+
+    @Override
+    public Actor process(JsonValue json, AssetDirectory assetDirectory, float scaleX, float scaleY, Actor parent) {
+        JsonValue data = json.get("data");
+        TextField.TextFieldStyle tfStyle = new TextField.TextFieldStyle();
+        tfStyle.font = assetDirectory.getEntry(data.getString("font"), BitmapFont.class);
+        JsonValue color = data.get("foreground");
+        if (color != null)
+            tfStyle.fontColor = new Color(color.getInt(0), color.getInt(1), color.getInt(2), color.getInt(3));
+        return new TextField(data.getString("text"), tfStyle);
+    }
+}
